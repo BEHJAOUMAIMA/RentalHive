2c4043d2cb6eafce3641421eccb6e54b6a2773f7
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-11-15 13:46:04 +0100

    add EquipmentRental & update other entities

--- src/main/java/com/example/rental_hive/domain/EquipmentRental.java
+++ src/main/java/com/example/rental_hive/domain/EquipmentRental.java
@@ -8,25 +8,32 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.ManyToOne;
 import javax.persistence.Id;
+import java.time.LocalDate;
+
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class EquipmentRental {
 
     @Id
     @GeneratedValue ( strategy = GenerationType.IDENTITY )
     private Long id;
 
-    private String fullName;
+    private LocalDate rentalStartDate;
 
-    private String email;
+    private LocalDate rentalEndDate;
 
-    private String mobile;
+    private Integer rentalDurationInDays;
 
-    private Boolean isAdmin;
+    @ManyToOne
+    private Equipment equipment;
+
+    @ManyToOne
+    private User user;
 
 }
