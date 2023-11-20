dce1776048e719aee5a7a57d309f876b56e83564
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-11-20 18:36:37 +0100

    add validaion to Equipment service methods

--- src/main/java/com/example/rental_hive/service/impl/EquipmentServiceImpl.java
+++ src/main/java/com/example/rental_hive/service/impl/EquipmentServiceImpl.java
@@ -6,7 +6,9 @@ import com.example.rental_hive.service.EquipmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.validation.Valid;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @Component
@@ -17,31 +19,84 @@ public class EquipmentServiceImpl implements EquipmentService {
 
 
     @Override
-    public Equipment save(Equipment equipment) {
+    public Equipment save(@Valid Equipment equipment) {
+
+        if (equipment.getId() != null){
+            throw new IllegalArgumentException("Equipment ID must be null for save operation.");
+        }
+        if (equipment.getName() == null || equipment.getName().trim().isEmpty()) {
+            throw new IllegalArgumentException("Equipment name cannot be null or empty.");
+        }
+        if (equipment.getSerialNumber() == null) {
+            throw new IllegalArgumentException("Serial number cannot be null.");
+        }
+        if (equipment.getCategory() == null) {
+            throw new IllegalArgumentException("Equipment category cannot be null.");
+        }
+        if (equipment.getQuantity() == null || equipment.getQuantity() <= 0) {
+            throw new IllegalArgumentException("Quantity must be a positive number.");
+        }
+        if (equipment.getRentalCost() == null || equipment.getRentalCost() <= 0) {
+            throw new IllegalArgumentException("Rental cost must be a positive number.");
+        }
         return equipmentRepository.save(equipment);
     }
 
     @Override
     public List<Equipment> findAll() {
-        return equipmentRepository.findAll();
+        List<Equipment> equipmentList = equipmentRepository.findAll();
+
+        if (equipmentList.isEmpty()) {
+            throw new NoSuchElementException("No equipment found.");
+        }
+
+        return equipmentList;
     }
 
     @Override
     public Optional<Equipment> findById(Long id) {
-        return equipmentRepository.findById(id);
+        Optional<Equipment> equipmentOptional = equipmentRepository.findById(id);
+
+        if (equipmentOptional.isEmpty()) {
+            throw new NoSuchElementException("Equipment not found with ID: " + id);
+        }
+
+        return equipmentOptional;
     }
 
     @Override
     public Equipment update(Equipment equipmentUpdated) {
-        Optional<Equipment> equipment = equipmentRepository.findById(equipmentUpdated.getId());
-        if (equipment.isPresent()){
-            return equipmentRepository.save(equipmentUpdated);
+        Long id = equipmentUpdated.getId();
+
+        Optional<Equipment> existingEquipmentOptional = equipmentRepository.findById(id);
+
+        if (existingEquipmentOptional.isPresent()) {
+            Equipment existingEquipment = existingEquipmentOptional.get();
+
+            if (!existingEquipment.getId().equals(equipmentUpdated.getId())) {
+                throw new IllegalArgumentException("Cannot update equipment with different ID.");
+            }
+
+            existingEquipment.setName(equipmentUpdated.getName());
+            existingEquipment.setSerialNumber(equipmentUpdated.getSerialNumber());
+            existingEquipment.setCategory(equipmentUpdated.getCategory());
+            existingEquipment.setQuantity(equipmentUpdated.getQuantity());
+            existingEquipment.setRentalCost(equipmentUpdated.getRentalCost());
+
+            return equipmentRepository.save(existingEquipment);
+        } else {
+            throw new NoSuchElementException("Equipment not found with ID: " + id);
         }
-        return null;
     }
 
     @Override
     public void delete(Long id) {
-        findById(id).ifPresent(equipment -> delete(equipment.getId()));
+        Optional<Equipment> equipmentOptional = findById(id);
+
+        if (equipmentOptional.isPresent()) {
+            equipmentRepository.delete(equipmentOptional.get());
+        } else {
+            throw new NoSuchElementException("Equipment not found with ID: " + id);
+        }
     }
 }
