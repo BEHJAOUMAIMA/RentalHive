8e1a2ff432c7fe6d0d7283d2b2f0c3b44a126795
BEHJAOUMAIMA <behja.oumaima1@gmail.com>
2023-11-20 09:16:28 +0100

    update user service

--- src/main/java/com/example/rental_hive/service/UserService.java
+++ src/main/java/com/example/rental_hive/service/UserService.java
@@ -1,19 +1,19 @@
 package com.example.rental_hive.service;
 
-import com.example.rental_hive.domain.Equipment;
 import com.example.rental_hive.domain.User;
-import com.example.rental_hive.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
 public interface UserService {
 
     public User save(User user);
 
     public List<User> findAll();
 
-    public User findById(Long id);
+    public Optional<User> findById(Long id);
 
     public User update(User userUpdated);
 
